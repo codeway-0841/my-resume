47fc21cde66f403d12afa2da29b66ba678a50fa3
codeway-0841 <joincoin.codeway@gmail.com>
2021-09-15 00:32:19 +0500

    update

--- src/data.js
+++ src/data.js
@@ -41,6 +41,72 @@ const projectsData = [
     ),
     link: "https://uzmdb.netlify.app/",
   },
+  {
+    title: "JOIN",
+    description: (
+      <div>
+        <p>
+          Joinchat is a freeware, cross-platform, cloud-based instant messaging
+          (IM) software and application service. Join messages are heavily
+          encrypted and can self-destruct. Synced. Telegram lets you access your
+          chats from multiple devices.
+        </p>
+      </div>
+    ),
+    link: "https://github.com/codeway-0841/join-frontend",
+  },
+  {
+    title: "DECOR STORE",
+    description: (
+      <div>
+        <p>
+          This home decor and art website specializes in one-of-a-kind antiques
+          and rare designer pieces, sourced from dealers all over the world. If
+          you have your heart set on a treasure from a particular era, start
+          your hunt here.
+        </p>
+      </div>
+    ),
+    link: "https://development-decor.vercel.app/",
+  },
+  {
+    title: "TODO",
+    description: (
+      <div>
+        <p>
+          The project utilises React to create a simple Todo app. Todo Items are
+          generated and rendered to the DOM on enter from the setFiltered State
+          instead of modifying and mutating the original array of Todos during
+          filtering.
+        </p>
+      </div>
+    ),
+    link: "https://codeway-0841.github.io/todo-with-redux/  ",
+  },
+  {
+    title: "JOINCOIN",
+    description: (
+      <div>
+        <p>
+          Your one-stop shop for crypto trading Trade Bitcoin, Ethereum, USDT,
+          and the top altcoins on the legendary crypto asset exchange.
+        </p>
+      </div>
+    ),
+    link: "https://codeway-0841.github.io/joincoin/",
+  },
+  {
+    title: "UZLOG",
+    description: (
+      <div>
+        <p>
+          Blog service for developers. Don't worry about where to write, start
+          with the blog.
+        </p>
+      </div>
+    ),
+    link: "",
+  },
 ];
 
 const skillsData = {
